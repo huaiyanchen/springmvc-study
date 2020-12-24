d2f8af048956929b936c6568ae9eb5c38d5086f5
chy <huaiyanchen@gmail.com>
2020-12-24 11:39:54 +0800

    Initial commit

---
+++ springmvc-01-servlet/src/main/java/com/chy/Person.java
@@ -0,0 +1,9 @@
+package com.chy;
+
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+}
---
+++ springmvc-01-servlet/src/main/java/com/chy/servlet.java
@@ -0,0 +1,8 @@
+package com.chy;
+
+import javax.servlet.http.HttpServlet;
+
+
+public class servlet extends HttpServlet  {
+
+}
